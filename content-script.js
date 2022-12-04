b612bbd5d603287cc064cf5cbcfafaf138439bc8
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-03 22:58:14 -0500

    Add getRanges function

--- content-script.js
+++ content-script.js
@@ -1,5 +1,16 @@
 const getSelection = () => window.getSelection();
 
+const getRanges = () => {
+	const selection = getSelection();
+	let rangeArray = [];
+
+	for (let i = 0; i < selection.rangeCount; i++) {
+		rangeArray[i] = selection.getRangeAt(i);
+	}
+
+	return rangeArray;
+};
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	console.log(message, sender, sendResponse);
 });
