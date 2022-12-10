174600454df9fe2ba8431be004a5ce9b02cf0d4b
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-09 22:01:14 -0500

    feat: set variables equal to import functions

--- content-script.js
+++ content-script.js
@@ -24,29 +24,36 @@ import("./range.js").then(({ getRangeArray, createRange }) => {
 	return newRange;
 }; */
 
-const createStyledSpan = (backgroundColor, spanClass) => {
+/* const createStyledSpan = (backgroundColor, spanClass) => {
 	const span = document.createElement("span");
 	span.classList.add("web-notes-highlight", spanClass);
 	span.style.backgroundColor = backgroundColor;
 	return span;
-};
+}; */
 
-const highlightText = (span, range) => {
+/* const highlightText = (span, range) => {
 	span.appendChild(range.extractContents());
 	return span;
-};
+}; */
 
-const insertHighlight = (range, span) => range.insertNode(span);
+/* const insertHighlight = (range, span) => range.insertNode(span); */
 
-const setHighlight = (range, spanClass) => {
+/* const setHighlight = (range, spanClass) => {
 	let span = createStyledSpan("red", spanClass);
 	highlightText(span, range);
 	insertHighlight(range, span);
-};
+}; */
 
-const getHighlights = () => {
+/* const getHighlights = () => {
 	return document.querySelectorAll(".web-notes-highlight");
-};
+}; */
+
+let setHighlight;
+let getHighlights;
+import("./highlight.js").then(({ setHighlightFunc, getHighlightsFunc }) => {
+	setHighlight = setHighlightFunc;
+	getHighlights = getHighlightsFunc;
+});
 
 const getNumberedClass = (highlight) => {
 	return highlight.className
